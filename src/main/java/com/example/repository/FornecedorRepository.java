413ec49417414f0b53c8ad467007149c2995dcc5
aleatorio101 <lukasdng@gmail.com>
2026-03-19 18:06:00 -0300

    criando repository de fornecedores

---
+++ src/main/java/com/example/repository/FornecedorRepository.java
@@ -0,0 +1,8 @@
+
+package com.example.repository;
+
+import com.example.entity.Fornecedor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FornecedorRepository extends JpaRepository<Fornecedor, Long> {
+}
